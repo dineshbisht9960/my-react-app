3e31c2ccc0851ec6d884ef25b57ed3a6d6e364f7
Dinesh Bisht <dineshbisht9960@gmail.com>
2026-02-16 17:45:54 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,10 @@
+import UserFormApi from "./components/UserFormApi";
+import ScannerPage from "./pages/ScannerPage";
+export  default function App(){
+  return(
+    <div>
+    <UserFormApi/>
+    {/* <ScannerPage/> */}
+    </div>
+  )
+}
